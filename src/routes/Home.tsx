8503954074480a2975d9709daa0113d06ae4c3c1
jaemin <als982001@naver.com>
2024-07-21 14:31:10 +0900

    메뉴바 생성

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,21 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase";
-
 export default function Home() {
-  const navigate = useNavigate();
-
-  const logOut = async () => {
-    try {
-      await auth.signOut();
-      navigate("/login");
-    } catch (e) {
-      console.log(`error: ${e}`);
-    }
-  };
-
-  return (
-    <div>
-      <button onClick={logOut}>Log out</button>
-    </div>
-  );
+  return <></>;
 }
