e1815671e24bbb3b9bfeba65eba2b5aab98ad643
jaemin <als982001@naver.com>
2024-07-21 14:50:52 +0900

    트윗 작성하는 폼 기본

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,13 @@
+import styled from "styled-components";
+
+import PostTweetForm from "../Components/PostTweetForm";
+
+const Wrapper = styled.div``;
+
 export default function Home() {
-  return <></>;
+  return (
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
+  );
 }
