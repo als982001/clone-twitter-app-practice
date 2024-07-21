da867c8340e7ab5166f1e40f3cf871397e1bdf43
jaemin <als982001@naver.com>
2024-07-21 17:01:34 +0900

    실시간 반영

--- src/Components/Timeline.tsx
+++ src/Components/Timeline.tsx
@@ -1,38 +1,58 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { Unsubscribe } from "firebase/auth";
 
 import { db } from "../firebase";
 import Tweet from "./Tweet";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+`;
 
 export default function Timeline() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
 
-  const fetchTweets = async () => {
-    const tweetQuery = query(
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    );
-
-    console.log(tweetQuery);
-
-    const snapshot = await getDocs(tweetQuery);
-
-    console.log(snapshot);
-
-    const tweets: ITweet[] = snapshot.docs.map((doc) => {
-      const { tweet, createdAt, userId, username, photo } = doc.data();
-
-      return { tweet, createdAt, userId, username, photo, id: doc.id };
-    });
-
-    setTweets(tweets);
-  };
-
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+
+    const fetchTweets = async () => {
+      const tweetQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+
+      unsubscribe = await onSnapshot(tweetQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+
+        setTweets(tweets);
+      });
+    };
+
     fetchTweets();
+
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
 
   return (
